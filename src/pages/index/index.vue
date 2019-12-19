13d3d20abb41aefd219274da730b31ae769dcc72
liuze <1573335091@qq.com>
2019-12-19 18:06:52 +0800

    123

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,36 +1,86 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-      不想写代码呀呀呀呀呀呀呀呀呀呀呀
-      我抗议呀呀呀呀呀呀
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+  <div>
+    <div class="top">
+      <div class="search ali-c">
+        <p>家乐福</p>
+        <div class="right flexc">
+          <img src="/static/images/index/search.png" alt="">
+          <span>搜索</span>
+        </div>
+      </div>
+      <swiper class="swiper" indicator-dots autoplay circular>
+        <swiper-item class="flexc">
+          <img src="/static/images/index/banner.png" alt="">
+        </swiper-item>
+      </swiper>
+    </div>
+    <div class="bai"></div>
+    <div class="icon flex">
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon1.png" alt="">
+          <p>天天特惠</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon2.png" alt="">
+          <p>动态文章</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon3.png" alt="">
+          <p>优惠券</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon4.png" alt="">
+          <p>商品分类</p>
+        </div>
       </div>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
+    <div class="youhui ali-c jus-b">
+      <img src="/static/images/index/qianggou.png" alt="">
+      <img src="/static/images/index/pintuan.png" alt="">
+    </div>
+    <div class="goods-box">
+      <div class="xd">
+        <div class="all flexc">
+          <img src="/static/images/index/title.png" alt="">
+        </div>
+        <div class="shai flex">
+          <div class="flex1 flexc">
+            <span>默认</span>
+          </div>
+          <div class="flex1 flexc">
+            <span>销量</span>
+            <img src="/static/images/index/shai.png" alt="">
+          </div>
+          <div class="flex1 flexc">
+            <span>价格</span>
+            <img src="/static/images/index/shai.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="goods jus-b flex-wrap">
+        <div class="list" v-for="(item, index) in goodsList" :key="index">
+          <img class="img" src="/static/images/index/goods.png" alt="">
+          <div class="text-box">
+            <p class="tit oneline">至本舒颜修护氨基酸洁至本舒颜修护氨基酸洁</p>
+            <div class="price">
+              <span>￥</span><span>29.00</span><span>￥39.00</span>
+            </div>
+            <div class="carda ali-c jus-b">
+              <span>已售1235</span>
+              <img src="/static/images/index/shop_card.png" alt="">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+    <img class="car" src="/static/images/index/card.png" alt="">
   </div>
 </template>
 
@@ -40,88 +90,172 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      goodsList:[,,,,,,],
     }
   },
-
-  components: {
-    card
-  },
-
+  
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
-  },
 
-  created () {
-    // let app = getApp()
   }
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+<style scoped lang='scss'>
+.xd{
+  position:sticky;
+  top: 0;
+  background-color: #fff;
+  box-shadow: 0 5rpx 8rpx 0rpx rgba($color: #000000, $alpha: 0.1)
+}
+// .xd{
+//   transition: all 0.3s;
+//   width:100%;
+//   height: 170rpx;
+//   background-color: #fff;
+// }
+// .isxd{
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+// }
+.car{
+  width: 98rpx;
+	height: 98rpx;
+  position: fixed;
+  bottom: 30rpx;
+  right: 30rpx;
+}
+.goods-box{
+  background-color: #fff;
+  margin-top: 20rpx;
+  .goods{
+    padding: 30rpx;
+    .list{
+      width: 335rpx;
+      height: 525rpx;
+      border-radius: 10rpx;
+      overflow: hidden;
+      margin-bottom: 20rpx;
+      box-shadow: 0rpx 2rpx 5rpx 0rpx rgba(0, 0, 0, 0.1);
+      .text-box{
+        padding: 0 20rpx;
+      }
+      .carda{
+        span{
+          font-size: 22rpx;
+          color: #999999;
+        }
+        img{
+          width: 50rpx;
+	        height: 50rpx;
+        }
+      }
+      .img{
+        width: 335rpx;
+	      height: 342rpx;
+      }
+      .tit{
+        color: #000000;
+        line-height: 70rpx;
+      }
+      .price span:nth-child(1){
+        font-size: 20rpx;
+        color: #ff3333;
+        font-weight: 900;
+      }
+      .price span:nth-child(2){
+        font-size: 36rpx;
+        color: #ff3333;
+        font-weight: 900;
+      }
+      .price span:nth-child(3){
+        font-size: 22rpx;
+        color: #999;
+        text-decoration:line-through;
+        margin-left: 15rpx;
+      }
+    }
+  }
+  .shai{
+    height: 70rpx;
+    color: #000;
+    font-size: 28rpx;
+    img{
+      width: 13rpx;
+	    height: 22rpx;
+      margin-left: 15rpx;
+    }
+  }
+  .all{
+    height: 100rpx;
+    img{
+      width: 378rpx;
+	    height: 32rpx;
+    }
+  }
+}
+.youhui{
+  background-color: #fff;
+  padding: 0 30rpx 40rpx;
+  img{
+    width: 335rpx;
+    height: 160rpx;
+    border-radius: 16rpx;
+  }
+}
+.icon{
+  background-color: #fff;
+  text-align: center;
+  height: 200rpx;
+  font-size: 26rpx;
+  p{
+    margin-top: -10rpx;
+  }
+  img{
+    width: 100rpx;
+	  height: 100rpx;
+  }
+}
+.bai{
+  height: 70rpx;
+  background-color: #fff
+}
+.top{
+  height: 320rpx;
+	background-image: linear-gradient(#ff3333, #fff);
+  position: relative;
+  .swiper{
+    height: 300rpx;
+    margin-top: 26rpx;
+    img{
+      width: 690rpx;
+      height: 300rpx;
+      border-radius: 20rpx;
+    }
+  }
+  .search{
+    height: 60rpx;
+    padding: 0 30rpx;
+    p{
+      font-size: 48rpx;
+      color: #fff;
+      margin-right: 50rpx;
+    }
+    .right{
+      width: 301rpx;
+	    height: 60rpx;
+      border-radius: 30rpx;
+      background-color: rgba($color: #fff, $alpha: 0.3);
+      img{
+        width: 28rpx;
+        height: 28rpx;
+        margin-right: 20rpx;
+      }
+      span{
+        color: #fff;
+      }
+    }
+  }
 }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -1,61 +1,261 @@
 <template>
   <div>
-      <swiper v-if="imgUrls.length > 0" indidator-dots="imgUrls.length > 1" >
-      <block v-for="(item, index) in imgUrls" :key="index" >
-        <swiper-item>
-          <image :src="item" mode="scaleToFill"></image>
+    <div class="top">
+      <div class="search ali-c">
+        <p>家乐福</p>
+        <div class="right flexc">
+          <img src="/static/images/index/search.png" alt="">
+          <span>搜索</span>
+        </div>
+      </div>
+      <swiper class="swiper" indicator-dots autoplay circular>
+        <swiper-item class="flexc">
+          <img src="/static/images/index/banner.png" alt="">
         </swiper-item>
-      </block>
-    </swiper>
-
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+      </swiper>
+    </div>
+    <div class="bai"></div>
+    <div class="icon flex">
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon1.png" alt="">
+          <p>天天特惠</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon2.png" alt="">
+          <p>动态文章</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon3.png" alt="">
+          <p>优惠券</p>
+        </div>
+      </div>
+      <div class="flex1 flexc">
+        <div>
+          <img src="/static/images/index/icon4.png" alt="">
+          <p>商品分类</p>
+        </div>
+      </div>
+    </div>
+    <div class="youhui ali-c jus-b">
+      <img src="/static/images/index/qianggou.png" alt="">
+      <img src="/static/images/index/pintuan.png" alt="">
+    </div>
+    <div class="goods-box">
+      <div class="xd">
+        <div class="all flexc">
+          <img src="/static/images/index/title.png" alt="">
+        </div>
+        <div class="shai flex">
+          <div class="flex1 flexc">
+            <span>默认</span>
+          </div>
+          <div class="flex1 flexc">
+            <span>销量</span>
+            <img src="/static/images/index/shai.png" alt="">
+          </div>
+          <div class="flex1 flexc">
+            <span>价格</span>
+            <img src="/static/images/index/shai.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="goods jus-b flex-wrap">
+        <div class="list" v-for="(item, index) in goodsList" :key="index">
+          <img class="img" src="/static/images/index/goods.png" alt="">
+          <div class="text-box">
+            <p class="tit oneline">至本舒颜修护氨基酸洁至本舒颜修护氨基酸洁</p>
+            <div class="price">
+              <span>￥</span><span>29.00</span><span>￥39.00</span>
+            </div>
+            <div class="carda ali-c jus-b">
+              <span>已售1235</span>
+              <img src="/static/images/index/shop_card.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <img class="car" src="/static/images/index/card.png" alt="">
   </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
 import card from '@/components/card'
 
 export default {
-  components: {
-    card
-  },
-
   data () {
     return {
-      logs: [],
-      imgUrls: [
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
-      ]
+      goodsList:[,,,,,,],
     }
   },
+  
+  methods: {
 
-  created () {
-    let logs
-    if (mpvuePlatform === 'my') {
-      logs = mpvue.getStorageSync({key: 'logs'}).data || []
-    } else {
-      logs = mpvue.getStorageSync('logs') || []
-    }
-    this.logs = logs.map(log => formatTime(new Date(log)))
   }
 }
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
+<style scoped lang='scss'>
+.xd{
+  position:sticky;
+  top: 0;
+  background-color: #fff;
+  box-shadow: 0 5rpx 8rpx 0rpx rgba($color: #000000, $alpha: 0.1)
+}
+// .xd{
+//   transition: all 0.3s;
+//   width:100%;
+//   height: 170rpx;
+//   background-color: #fff;
+// }
+// .isxd{
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+// }
+.car{
+  width: 98rpx;
+	height: 98rpx;
+  position: fixed;
+  bottom: 30rpx;
+  right: 30rpx;
+}
+.goods-box{
+  background-color: #fff;
+  margin-top: 20rpx;
+  .goods{
+    padding: 30rpx;
+    .list{
+      width: 335rpx;
+      height: 525rpx;
+      border-radius: 10rpx;
+      overflow: hidden;
+      margin-bottom: 20rpx;
+      box-shadow: 0rpx 2rpx 5rpx 0rpx rgba(0, 0, 0, 0.1);
+      .text-box{
+        padding: 0 20rpx;
+      }
+      .carda{
+        span{
+          font-size: 22rpx;
+          color: #999999;
+        }
+        img{
+          width: 50rpx;
+	        height: 50rpx;
+        }
+      }
+      .img{
+        width: 335rpx;
+	      height: 342rpx;
+      }
+      .tit{
+        color: #000000;
+        line-height: 70rpx;
+      }
+      .price span:nth-child(1){
+        font-size: 20rpx;
+        color: #ff3333;
+        font-weight: 900;
+      }
+      .price span:nth-child(2){
+        font-size: 36rpx;
+        color: #ff3333;
+        font-weight: 900;
+      }
+      .price span:nth-child(3){
+        font-size: 22rpx;
+        color: #999;
+        text-decoration:line-through;
+        margin-left: 15rpx;
+      }
+    }
+  }
+  .shai{
+    height: 70rpx;
+    color: #000;
+    font-size: 28rpx;
+    img{
+      width: 13rpx;
+	    height: 22rpx;
+      margin-left: 15rpx;
+    }
+  }
+  .all{
+    height: 100rpx;
+    img{
+      width: 378rpx;
+	    height: 32rpx;
+    }
+  }
+}
+.youhui{
+  background-color: #fff;
+  padding: 0 30rpx 40rpx;
+  img{
+    width: 335rpx;
+    height: 160rpx;
+    border-radius: 16rpx;
+  }
+}
+.icon{
+  background-color: #fff;
+  text-align: center;
+  height: 200rpx;
+  font-size: 26rpx;
+  p{
+    margin-top: -10rpx;
+  }
+  img{
+    width: 100rpx;
+	  height: 100rpx;
+  }
+}
+.bai{
+  height: 70rpx;
+  background-color: #fff
+}
+.top{
+  height: 320rpx;
+	background-image: linear-gradient(#ff3333, #fff);
+  position: relative;
+  .swiper{
+    height: 300rpx;
+    margin-top: 26rpx;
+    img{
+      width: 690rpx;
+      height: 300rpx;
+      border-radius: 20rpx;
+    }
+  }
+  .search{
+    height: 60rpx;
+    padding: 0 30rpx;
+    p{
+      font-size: 48rpx;
+      color: #fff;
+      margin-right: 50rpx;
+    }
+    .right{
+      width: 301rpx;
+	    height: 60rpx;
+      border-radius: 30rpx;
+      background-color: rgba($color: #fff, $alpha: 0.3);
+      img{
+        width: 28rpx;
+        height: 28rpx;
+        margin-right: 20rpx;
+      }
+      span{
+        color: #fff;
+      }
+    }
+  }
 }
 
-.log-item {
-  margin: 10rpx;
-}
 </style>
